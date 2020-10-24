48525960f6a46033bf10909642cfa7da272d5a57
Biterider <Biterider@gmx.net>
2020-10-24 10:59:25 +0200

    Arduino COM polished

--- Projects/X/Arduino COM/ArduinoComTest/ArduinoComTest.ino
+++ Projects/X/Arduino COM/ArduinoComTest/ArduinoComTest.ino
@@ -1,21 +1,15 @@
 // ************************************************************************************************
 // Project:     ArduinoComTest
-// Description: Arduino - PC communication test
+// Description: Arduino - PC ObjAsm communication test
 // Author:      G. Friedrich
 // Version:     1.0.0, October 2020
 // Docs:        https://www.arduino.cc/reference/en/language/functions/communication/serial/
 
 
-// **** Globals switches **************************************************************************
-#define DEBUGGING                                       // Use Serial to show debug information
-
-
 // **** Code **************************************************************************************
 void setup() {
-  #ifdef DEBUGGING
-    Serial.begin(9600, SERIAL_8N1);                     // Setup serial COM Port
-    while (!Serial) {}                                  // Wait for serial port to connect
-  #endif                                                // Needed for native USB port only
+  Serial.begin(9600, SERIAL_8N1);                       // Setup serial COM Port
+  while (!Serial) {}                                    // Wait for serial port to connect
   //Serial.println("Arduino ready");
 }
 
