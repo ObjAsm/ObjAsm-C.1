a43511a30477d01517daa089a6db2f3e4e264cc1
Biterider <Biterider@gmx.net>
2021-09-15 22:58:02 +0200

    Chart update

---
+++ Code/Objects/ChartXYSetup.asm
@@ -0,0 +1,30 @@
+; ==================================================================================================
+; Title:      ChartXYSetup.asm
+; Author:     G. Friedrich
+; Version:    C.1.0
+; Purpose:    ObjAsm compilation file for ChartXYSetup object.
+; Notes:      Version C.1.0, August 2021
+;             - First release.
+; ==================================================================================================
+
+
+% include Objects.cop
+
+% include &MacPath&fMath.inc
+
+;Add here all files that build the inheritance path and referenced objects
+LoadObjects Primer
+LoadObjects Stream
+LoadObjects WinPrimer
+LoadObjects Window
+LoadObjects Dialog
+LoadObjects DialogModal
+LoadObjects Array
+LoadObjects Collection
+LoadObjects ChartFlat
+LoadObjects ChartXY
+
+;Add here the file that defines the object(s) to be included in the library
+MakeObjects ChartXYSetup
+
+end
