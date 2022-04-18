c0eb27fec9d100a5e83a603352eab2e2e726d72f
Biterider <Biterider@gmx.net>
2022-04-18 08:53:52 +0200

    IsProcessElevated.asm added

---
+++ Code/ObjMem/32/IsProcessElevated.asm
@@ -0,0 +1,13 @@
+; ==================================================================================================
+; Title:      IsProcessElevated.asm
+; Author:     G. Friedrich
+; Version:    C.1.0
+; Notes:      Version C.1.0, April 2022.
+;               - First release.
+; ==================================================================================================
+
+
+% include @Environ(OBJASM_PATH)\\Code\\OA_Setup32.inc
+% include &ObjMemPath&X\IsProcessElevated.asm
+
+end
---
+++ Code/ObjMem/64/IsProcessElevated.asm
@@ -0,0 +1,13 @@
+; ==================================================================================================
+; Title:      IsProcessElevated.asm
+; Author:     G. Friedrich
+; Version:    C.1.0
+; Notes:      Version C.1.0, April 2022.
+;               - First release.
+; ==================================================================================================
+
+
+% include @Environ(OBJASM_PATH)\\Code\\OA_Setup64.inc
+% include &ObjMemPath&X\IsProcessElevated.asm
+
+end
