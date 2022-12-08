3ac77a37cdb2cdcc8475bf0e5c1fc1749e4de332
Biterider <Biterider@gmx.net>
2022-12-08 22:16:05 +0100

    Synch2 C.2

--- Build/Make.cmd
+++ Build/Make.cmd
@@ -3,15 +3,10 @@ REM Usage:
 REM   1. copy this file to the destination folder.
 REM   2. execute it to to start the build process.
 
-set LogFile=Make.log
-if exist "%OBJASM_PATH%1" (
+if exist "%OBJASM_PATH%" (
   call "%OBJASM_PATH%\Build\OA_BUILD.cmd" %*
-  ) else (
-    echo Incorrect Path to ObjAsm
-    if not [!LogFile!] == [] (
-      echo Incorrect Path to ObjAsm> !LogFile!
-    )
-    exit /b 1
-  )
+) else (
+  echo Wrong path to ObjAsm. Check your OBJASM_PATH environment variable.
+  if not "%1" equ "NOPAUSE" pause
+  exit /b 1
 )
-
